1d5356fa354d39dbdaf03aa141c99402146ce5b7
Meharbi88 <32585759+Meharbi88@users.noreply.github.com>
2017-10-06 21:57:59 -0500

    TEXTVIEW CHANGE
    FAHAD ALHARBI

--- FinalProject/app/src/main/java/edu/depaul/csc472/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/edu/depaul/csc472/finalproject/MainActivity.java
@@ -2,6 +2,7 @@ package edu.depaul.csc472.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView tx = (TextView) findViewById(R.id.myText);
+
+        tx.setText("FAHAD ALHARBI");
+
     }
 }
