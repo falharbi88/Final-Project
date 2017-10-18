058abe55d09c7992d0c306dd6518f10279b65a55
Meharbi88 <32585759+Meharbi88@users.noreply.github.com>
2017-10-17 19:11:51 -0500

    adding button in cart page
    adding button in cart page

--- FinalProject/app/src/main/java/edu/depaul/csc472/finalproject/CartActivity.java
+++ FinalProject/app/src/main/java/edu/depaul/csc472/finalproject/CartActivity.java
@@ -1,19 +1,23 @@
 package edu.depaul.csc472.finalproject;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class CartActivity extends AppCompatActivity {
-
+    public boolean flag = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
-        //Button submitButton = (Button) findViewById(R.id.SubmitButton);
+        Button submitButton = (Button) findViewById(R.id.SubmitButton);
         TextView textSummary = (TextView) findViewById(R.id.TextSummary);
         TextView mealsSummary = (TextView) findViewById(R.id.MealsSummary);
         TextView mealsPrices = (TextView) findViewById(R.id.MealsPrices);
@@ -24,6 +28,7 @@ public class CartActivity extends AppCompatActivity {
         //String st = "";
         //String my = "";
         if(!MenuActivity.myOrders.isEmpty()){
+            flag=true;
             textSummary.setText("Your Order is: \n");
             for (int i=0; i< MenuActivity.myOrders.size(); i++){
                 mealsSummary.setText(mealsSummary.getText().toString()+"Meal "+String.valueOf(i+1)+ ":\n");
@@ -35,6 +40,19 @@ public class CartActivity extends AppCompatActivity {
             }
             totalPrice.setText("$"+String.valueOf(sum));
         }
+
+//        submitButton.setOnClickListener(new View.OnClickListener(){
+//
+//
+//            @Override
+//            public void onClick(View view) {
+//                if(flag){
+//                    final ProgressDialog myDialog = new ProgressDialog(CartActivity.this);
+//                    myDialog.setMessage("Your Order has bee submitted");
+//                    myDialog.show();
+//                }
+//            }
+//        });
 
 
     }
