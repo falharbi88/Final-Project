0eaec78740944f37f841f51a54851ce7b5e720d5
Meharbi88 <32585759+Meharbi88@users.noreply.github.com>
2017-11-04 02:55:34 -0500

    adding focus view for sign up and sign in

--- FinalProject/app/src/main/java/edu/depaul/csc472/finalproject/SignIn.java
+++ FinalProject/app/src/main/java/edu/depaul/csc472/finalproject/SignIn.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,40 +38,61 @@ public class SignIn extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final ProgressDialog myDialog = new ProgressDialog(SignIn.this);
-                myDialog.setMessage("Please waiting....");
-                myDialog.show();
-                table_user.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        if(dataSnapshot.child(usernameInput.getText().toString()).exists()){
-                            myDialog.dismiss();
-                            User user = dataSnapshot.child(usernameInput.getText().toString()).getValue(User.class);
-                            if(user.getPassword().equals(passwordInput.getText().toString())) {
-                                Toast.makeText(SignIn.this, "Sign in successfully", Toast.LENGTH_LONG).show();
-                                Intent homeActivity = new Intent(SignIn.this, HomeActivity.class);
-                                homeActivity.putExtra("Username",usernameInput.getText().toString());
-                                startActivity(homeActivity);
-                            }else
-                                Toast.makeText(SignIn.this, "Sign in Failed!!!!!", Toast.LENGTH_LONG).show();
-                        }else{
-                            myDialog.dismiss();
-                            Toast.makeText(SignIn.this,"User not Found", Toast.LENGTH_LONG).show();
+                if (checkFields()) {
+                    final ProgressDialog myDialog = new ProgressDialog(SignIn.this);
+                    myDialog.setMessage("Please waiting....");
+                    myDialog.show();
+
+                    table_user.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.child(usernameInput.getText().toString()).exists()) {
+                                myDialog.dismiss();
+                                User user = dataSnapshot.child(usernameInput.getText().toString()).getValue(User.class);
+                                if (user.getPassword().equals(passwordInput.getText().toString())) {
+                                    Toast.makeText(SignIn.this, "Sign in successfully", Toast.LENGTH_LONG).show();
+                                    Intent homeActivity = new Intent(SignIn.this, HomeActivity.class);
+                                    homeActivity.putExtra("Username", usernameInput.getText().toString());
+                                    startActivity(homeActivity);
+                                } else
+                                    Toast.makeText(SignIn.this, "Sign in Failed!!!!!", Toast.LENGTH_LONG).show();
+                            } else {
+                                myDialog.dismiss();
+                                Toast.makeText(SignIn.this, "User not Found", Toast.LENGTH_LONG).show();
+
+                            }
+
 
                         }
 
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                        }
+                    });
+                }else{
+                    Toast.makeText(SignIn.this, "Please Complete All Fields", Toast.LENGTH_LONG).show();
 
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
+                }
             }
         });
 
     }
 
+
+    public boolean checkFields() {
+        View focusView=null;
+        if (TextUtils.isEmpty(usernameInput.getText().toString())) {
+            usernameInput.setError(getString(R.string.error_field_required));
+            focusView = usernameInput;
+            Toast.makeText(this, "Username can't be empty", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (TextUtils.isEmpty(passwordInput.getText().toString())) {
+            passwordInput.setError(getString(R.string.error_field_required));
+            focusView = passwordInput;
+            Toast.makeText(this, "Password can't be empty", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
 
 }
--- FinalProject/app/src/main/java/edu/depaul/csc472/finalproject/SignUp.java
+++ FinalProject/app/src/main/java/edu/depaul/csc472/finalproject/SignUp.java
@@ -3,6 +3,7 @@ package edu.depaul.csc472.finalproject;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +14,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import edu.depaul.csc472.finalproject.Model.User;
 
@@ -35,25 +39,22 @@ public class SignUp extends AppCompatActivity {
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference table_user = database.getReference("User");
-        final boolean check = usernameInput.getText().toString().isEmpty()||emailInput.getText().toString().isEmpty()||
-                firstNameInput.getText().toString().isEmpty()||lastNameInput.getText().toString().isEmpty()||
-                passwordInput.getText().toString().isEmpty();
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!check) {
+                if (checkFields()) {
                 final ProgressDialog myDialog = new ProgressDialog(SignUp.this);
                 myDialog.setMessage("Please waiting....");
                 myDialog.show();
                 table_user.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        if (!dataSnapshot.child(usernameInput.getText().toString()).exists()) {
+                        if (!(dataSnapshot.child(usernameInput.getText().toString()).exists())) {
                             myDialog.dismiss();
                             User newUser = new User(emailInput.getText().toString(), firstNameInput.getText().toString(), lastNameInput.getText().toString(), passwordInput.getText().toString());
                             table_user.child(usernameInput.getText().toString()).setValue(newUser);
-                            Toast.makeText(SignUp.this, "Sign up successfully", Toast.LENGTH_LONG).show();
+                            Toast.makeText(SignUp.this, newUser.toString(), Toast.LENGTH_LONG).show();
                             finish();
                         } else {
                             myDialog.dismiss();
@@ -77,5 +78,50 @@ public class SignUp extends AppCompatActivity {
 
 
 
+    }
+
+    public boolean checkFields() {
+        View focusView=null;
+        if (TextUtils.isEmpty(usernameInput.getText().toString())) {
+            usernameInput.setError(getString(R.string.error_field_required));
+            focusView = usernameInput;
+            Toast.makeText(this, "Username can't be empty", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (TextUtils.isEmpty(passwordInput.getText().toString())) {
+            passwordInput.setError(getString(R.string.error_field_required));
+            focusView = passwordInput;
+            Toast.makeText(this, "Password can't be empty", Toast.LENGTH_LONG).show();
+            return false;
+        }else if (passwordInput.getText().length()<=8) {
+            passwordInput.setError("Password length must be greater than 8 characters");
+            focusView = passwordInput;
+            return false;
+        } else if (TextUtils.isEmpty(firstNameInput.getText().toString())) {
+            firstNameInput.setError(getString(R.string.error_field_required));
+            focusView = firstNameInput;
+            Toast.makeText(this, "First name can't be empty", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (TextUtils.isEmpty(lastNameInput.getText().toString())) {
+            lastNameInput.setError(getString(R.string.error_field_required));
+            focusView = lastNameInput;
+            Toast.makeText(this, "Last name can't be empty", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (TextUtils.isEmpty(emailInput.getText().toString())) {
+            emailInput.setError(getString(R.string.error_field_required));
+            focusView = emailInput;
+            Toast.makeText(this, "Email name can't be empty", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (!isEmailValid(emailInput.getText().toString())) {
+            emailInput.setError("Invalid email format");
+            focusView = emailInput;
+            return false;
+        }
+        return true;
+    }
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
